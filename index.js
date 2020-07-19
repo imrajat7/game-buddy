a97cd794d4ce0b3157e2fb087264e8df9fc455d2
Paras Gupta <parasg1999@gmail.com>
2020-07-19 21:37:50 +0530

    Razorpay test added

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 
-const stripe = require("stripe")("sk_test_51H5oBzGGRkVJlDCl2lf2c4An9AVcSvDlTSRwk3fYOx3RE5zGREhIeYVmWhUhDD8TA0okmNbRBIvfHSEh54VYYz9W00XmlgWOll");
-
 require('dotenv').config();
 
 const app = express();
@@ -39,24 +37,12 @@ mongoose
 
 app.use('/auth', require('./routes/auth'));
 app.use('/room', require('./routes/room'));
-
-app.post("/create-payment-intent", async (req, res) => {
-    const { items } = req.body;
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: "1000",
-      currency: "inr"
-    });
-    console.log(paymentIntent);
-    res.send({
-      clientSecret: paymentIntent.client_secret
-    });
-  });
+app.use('/payment', require('./routes/payment'));
 
 app.get('/', (req, res) => {
     // Test for rendering pages
     res.render('checkout');
-})
+});
 
 const PORT = process.env.PORT || 8000;
 
