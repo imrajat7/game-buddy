540db5945e1134559ada02bcca58faba2bb5fe2a
Paras Gupta <parasg1999@gmail.com>
2020-07-24 00:49:13 +0530

    Fix bugs

--- routes/user.js
+++ routes/user.js
@@ -8,13 +8,14 @@ router.get('/moreDetails', isUserLoggedIn, (req, res) => {
     if (req.user.isRegistered) {
         return res.redirect('/dashboard');
     }
-    return res.render('getMoreDetails', {user: req.user});
+    return res.render('getMoreDetails', { user: req.user });
 });
 
 router.post('/moreDetails', isUserLoggedIn, (req, res) => {
     const { name, pubgUsername, referral } = req.body;
-    
-    const role = [917889608162,
+
+    const role = [
+        917889608162,
         919468344399,
         917006903831,
         917018633907,
@@ -30,17 +31,17 @@ router.post('/moreDetails', isUserLoggedIn, (req, res) => {
 
 router.get('/profile', isUserLoggedIn, (req, res) => {
     User.findById(req.user._id)
-    .then(user => res.render('profile', {user}))
-    .catch(err => res.status(400).send({ err }))
+        .then(user => res.render('profile', { user }))
+        .catch(err => res.status(400).send({ err }))
 });
 
 router.post('/update', isUserLoggedIn, (req, res) => {
-    const {name, pubgUsername} = req.body;
+    const { name, pubgUsername } = req.body;
     User.findByIdAndUpdate(req.user._id, {
         name, pubgUsername,
     })
-    .then(user => res.redirect('/user/profile'))
-    .catch(err => res.status(400).send({ err }));
+        .then(user => res.redirect('/user/profile'))
+        .catch(err => res.status(400).send({ err }));
 })
 
 module.exports = router;
